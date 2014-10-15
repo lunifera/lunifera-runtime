2310f2e7e6f98d3121d05e7bdf1ea0cb647bb57f
Florian Pirchner <florian.pirchner@gmail.com>
2014-10-15 07:02:06 +0200

    Added new Annotations

---
+++ org.lunifera.runtime.common/src/org/lunifera/runtime/common/annotations/DomainEmbedded.java
@@ -0,0 +1,29 @@
+/**
+ * Copyright (c) 2012 Committers of lunifera.org.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *    Florian Pirchner - initial API and implementation
+ */
+package org.lunifera.runtime.common.annotations;
+
+import static java.lang.annotation.ElementType.FIELD;
+import static java.lang.annotation.ElementType.METHOD;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+/**
+ * This annotation marks fields in JPA entities and DTOs, that the content is
+ * embedded. So UIs do not have to provide search fields for it, but need to
+ * inline its fields into a form.
+ */
+@Target({ FIELD, METHOD })
+@Retention(RUNTIME)
+public @interface DomainEmbedded {
+
+}
