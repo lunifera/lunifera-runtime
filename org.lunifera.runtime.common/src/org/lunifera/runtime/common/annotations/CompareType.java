c42f225cbcfc1885d3d2040f4b1c38cb58014edb
Florian Pirchner <florian.pirchner@gmail.com>
2015-02-16 17:18:18 +0100

    TargetEnumConstraints for filter

---
+++ org.lunifera.runtime.common/src/org/lunifera/runtime/common/annotations/CompareType.java
@@ -0,0 +1,303 @@
+/**
+ * Copyright (c) 2012 Committers of lunifera.org.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *    Florian Pirchner - initial API and implementation
+ */
+package org.lunifera.runtime.common.annotations;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public enum CompareType {
+	/**
+	 * The '<em><b>EQUALS</b></em>' literal object. <!-- begin-user-doc --> <!--
+	 * end-user-doc -->
+	 * 
+	 * @see #EQUALS_VALUE
+	 * @generated
+	 * @ordered
+	 */
+	EQUALS(0, "EQUALS", "=="),
+
+	/**
+	 * The '<em><b>GREATER</b></em>' literal object. <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * 
+	 * @see #GREATER_VALUE
+	 * @generated
+	 * @ordered
+	 */
+	GREATER(0, "GREATER", ">"),
+
+	/**
+	 * The '<em><b>LOWER</b></em>' literal object. <!-- begin-user-doc --> <!--
+	 * end-user-doc -->
+	 * 
+	 * @see #LOWER_VALUE
+	 * @generated
+	 * @ordered
+	 */
+	LOWER(0, "LOWER", "<"),
+
+	/**
+	 * The '<em><b>GREATER EQ</b></em>' literal object. <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * 
+	 * @see #GREATER_EQ_VALUE
+	 * @generated
+	 * @ordered
+	 */
+	GREATER_EQ(0, "GREATER_EQ", ">="),
+
+	/**
+	 * The '<em><b>LOWER EQ</b></em>' literal object. <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * 
+	 * @see #LOWER_EQ_VALUE
+	 * @generated
+	 * @ordered
+	 */
+	LOWER_EQ(0, "LOWER_EQ", "<="),
+
+	/**
+	 * The '<em><b>NOT EQ</b></em>' literal object. <!-- begin-user-doc --> <!--
+	 * end-user-doc -->
+	 * 
+	 * @see #NOT_EQ_VALUE
+	 * @generated
+	 * @ordered
+	 */
+	NOT_EQ(0, "NOT_EQ", "<>");
+
+	/**
+	 * The '<em><b>EQUALS</b></em>' literal value. <!-- begin-user-doc -->
+	 * <p>
+	 * If the meaning of '<em><b>EQUALS</b></em>' literal object isn't clear,
+	 * there really should be more of a description here...
+	 * </p>
+	 * <!-- end-user-doc -->
+	 * 
+	 * @see #EQUALS
+	 * @model literal="=="
+	 * @generated
+	 * @ordered
+	 */
+	public static final int EQUALS_VALUE = 0;
+
+	/**
+	 * The '<em><b>GREATER</b></em>' literal value. <!-- begin-user-doc -->
+	 * <p>
+	 * If the meaning of '<em><b>GREATER</b></em>' literal object isn't clear,
+	 * there really should be more of a description here...
+	 * </p>
+	 * <!-- end-user-doc -->
+	 * 
+	 * @see #GREATER
+	 * @model literal=">"
+	 * @generated
+	 * @ordered
+	 */
+	public static final int GREATER_VALUE = 0;
+
+/**
+	 * The '<em><b>LOWER</b></em>' literal value.
+	 * <!-- begin-user-doc -->
+	 * <p>
+	 * If the meaning of '<em><b>LOWER</b></em>' literal object isn't clear,
+	 * there really should be more of a description here...
+	 * </p>
+	 * <!-- end-user-doc -->
+	 * @see #LOWER
+	 * @model literal="<"
+	 * @generated
+	 * @ordered
+	 */
+	public static final int LOWER_VALUE = 0;
+
+	/**
+	 * The '<em><b>GREATER EQ</b></em>' literal value. <!-- begin-user-doc -->
+	 * <p>
+	 * If the meaning of '<em><b>GREATER EQ</b></em>' literal object isn't
+	 * clear, there really should be more of a description here...
+	 * </p>
+	 * <!-- end-user-doc -->
+	 * 
+	 * @see #GREATER_EQ
+	 * @model literal=">="
+	 * @generated
+	 * @ordered
+	 */
+	public static final int GREATER_EQ_VALUE = 0;
+
+	/**
+	 * The '<em><b>LOWER EQ</b></em>' literal value. <!-- begin-user-doc -->
+	 * <p>
+	 * If the meaning of '<em><b>LOWER EQ</b></em>' literal object isn't clear,
+	 * there really should be more of a description here...
+	 * </p>
+	 * <!-- end-user-doc -->
+	 * 
+	 * @see #LOWER_EQ
+	 * @model literal="<="
+	 * @generated
+	 * @ordered
+	 */
+	public static final int LOWER_EQ_VALUE = 0;
+
+	/**
+	 * The '<em><b>NOT EQ</b></em>' literal value. <!-- begin-user-doc -->
+	 * <p>
+	 * If the meaning of '<em><b>NOT EQ</b></em>' literal object isn't clear,
+	 * there really should be more of a description here...
+	 * </p>
+	 * <!-- end-user-doc -->
+	 * 
+	 * @see #NOT_EQ
+	 * @model literal="<>"
+	 * @generated
+	 * @ordered
+	 */
+	public static final int NOT_EQ_VALUE = 0;
+
+	/**
+	 * An array of all the '<em><b>LComparator Type</b></em>' enumerators. <!--
+	 * begin-user-doc --> <!-- end-user-doc -->
+	 * 
+	 * @generated
+	 */
+	private static final CompareType[] VALUES_ARRAY = new CompareType[] {
+			EQUALS, GREATER, LOWER, GREATER_EQ, LOWER_EQ, NOT_EQ, };
+
+	/**
+	 * A public read-only list of all the '<em><b>LComparator Type</b></em>'
+	 * enumerators. <!-- begin-user-doc --> <!-- end-user-doc -->
+	 * 
+	 * @generated
+	 */
+	public static final List<CompareType> VALUES = Collections
+			.unmodifiableList(Arrays.asList(VALUES_ARRAY));
+
+	/**
+	 * Returns the '<em><b>LComparator Type</b></em>' literal with the specified
+	 * literal value. <!-- begin-user-doc --> <!-- end-user-doc -->
+	 * 
+	 * @generated
+	 */
+	public static CompareType get(String literal) {
+		for (int i = 0; i < VALUES_ARRAY.length; ++i) {
+			CompareType result = VALUES_ARRAY[i];
+			if (result.toString().equals(literal)) {
+				return result;
+			}
+		}
+		return null;
+	}
+
+	/**
+	 * Returns the '<em><b>LComparator Type</b></em>' literal with the specified
+	 * name. <!-- begin-user-doc --> <!-- end-user-doc -->
+	 * 
+	 * @generated
+	 */
+	public static CompareType getByName(String name) {
+		for (int i = 0; i < VALUES_ARRAY.length; ++i) {
+			CompareType result = VALUES_ARRAY[i];
+			if (result.getName().equals(name)) {
+				return result;
+			}
+		}
+		return null;
+	}
+
+	/**
+	 * Returns the '<em><b>LComparator Type</b></em>' literal with the specified
+	 * integer value. <!-- begin-user-doc --> <!-- end-user-doc -->
+	 * 
+	 * @generated
+	 */
+	public static CompareType get(int value) {
+		switch (value) {
+		case EQUALS_VALUE:
+			return EQUALS;
+		}
+		return null;
+	}
+
+	/**
+	 * <!-- begin-user-doc --> <!-- end-user-doc -->
+	 * 
+	 * @generated
+	 */
+	private final int value;
+
+	/**
+	 * <!-- begin-user-doc --> <!-- end-user-doc -->
+	 * 
+	 * @generated
+	 */
+	private final String name;
+
+	/**
+	 * <!-- begin-user-doc --> <!-- end-user-doc -->
+	 * 
+	 * @generated
+	 */
+	private final String literal;
+
+	/**
+	 * Only this class can construct instances. <!-- begin-user-doc --> <!--
+	 * end-user-doc -->
+	 * 
+	 * @generated
+	 */
+	private CompareType(int value, String name, String literal) {
+		this.value = value;
+		this.name = name;
+		this.literal = literal;
+	}
+
+	/**
+	 * <!-- begin-user-doc --> <!-- end-user-doc -->
+	 * 
+	 * @generated
+	 */
+	public int getValue() {
+		return value;
+	}
+
+	/**
+	 * <!-- begin-user-doc --> <!-- end-user-doc -->
+	 * 
+	 * @generated
+	 */
+	public String getName() {
+		return name;
+	}
+
+	/**
+	 * <!-- begin-user-doc --> <!-- end-user-doc -->
+	 * 
+	 * @generated
+	 */
+	public String getLiteral() {
+		return literal;
+	}
+
+	/**
+	 * Returns the literal value of the enumerator, which is its string
+	 * representation. <!-- begin-user-doc --> <!-- end-user-doc -->
+	 * 
+	 * @generated
+	 */
+	@Override
+	public String toString() {
+		return literal;
+	}
+
+} // CompareType
