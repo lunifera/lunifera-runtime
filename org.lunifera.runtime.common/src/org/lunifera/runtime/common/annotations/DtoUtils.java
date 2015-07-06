7117aacfb8b351999c200936cf701bd5311b15a5
Sabrina Hopf <sabrina.hopf@lunifera.com>
2015-07-06 12:02:49 +0200

    Added copyright notices

--- org.lunifera.runtime.common/src/org/lunifera/runtime/common/annotations/DtoUtils.java
+++ org.lunifera.runtime.common/src/org/lunifera/runtime/common/annotations/DtoUtils.java
@@ -1,12 +1,12 @@
 /**
- * Copyright (c) 2011 - 2014, Lunifera GmbH (Gross Enzersdorf), Loetz KG (Heidelberg)
+ * Copyright (c) 2011 - 2015, Lunifera GmbH (Gross Enzersdorf), Loetz KG (Heidelberg)
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
- * 
- * Contributors: 
- * 		Florian Pirchner - Initial implementation
+ *
+ * Contributors:
+ *         Florian Pirchner - Initial implementation
  */
 package org.lunifera.runtime.common.annotations;
 
